5bce01f8fa983ed71a0abd96bcb75906b5a2f10b
Mike Oberhausen <michael.oberhausen@yahoo.com>
2022-02-05 15:27:51 -0500

    UDP file update, move to python3, some try/except blocks added, added dynamic IP fetching...

--- src/UDPClass.py
+++ src/UDPClass.py
@@ -1,23 +1,46 @@
 import socket
 import garagedoor
 
-#TODO: Dynamically get the IP address of the machine
-UDP_IP = "192.168.1.140"
+#Gets the IP address of the host machine
+def get_my_IP_Address():
+	BROADCAST_IP = '10.255.255.255'
+	try:
+		host_ip = socket.gethostbyname(socket.gethostname())
+	except:
+		host_ip = '-1'
+
+	if host_ip.startswith("127.0"):
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s.connect((BROADCAST_IP, 1))
+		host_ip = s.getsockname()[0]
+	return host_ip
+
+UDP_IP = get_my_IP_Address()
+#check if the UDP_IP was fetched correctly. If not, exit gracefully.
+#if(UDP_IP == '-1'):
+#	print("Problem with fetching host IP address, exiting")
+#	return
+
 UDP_Port = 2523
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#TODO: Try-Catch here in case we are already running. Avoid a crashing
-sock.bind((UDP_IP, UDP_Port))
+#Try-Catch here in case we are already running. Avoid a crashing
+try:
+	sock.bind((UDP_IP, UDP_Port))
+except Exception as e:
+	print("Problem opening IP/Port combo")
+	print(repr(e))
 
-gd = garagedoor.GarageDoor("Open Sesame")
+gd = garagedoor.GarageDoor(b'Open Sesame')
 
 while True: #TODO: There has to be a better way to do this
 	data, addr = sock.recvfrom(1024)
-	print addr
-	print data
+	print(addr)
+	print(data)
 
 	gd.Actuate(data)
+
 
 
 
